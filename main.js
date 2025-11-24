b20334534ae048fa87743e7b7d5c6af17becd8e6
weicheng <weicheng199610@gmail.com>
2025-11-24 10:03:19 +0800

    Squashed 'my-electron-app/' content from commit 2b33b93
    git-subtree-dir: my-electron-app
    git-subtree-split: 2b33b930f91965e9ea40b80a027e88fd6fdeaef8

---
+++ main.js
@@ -0,0 +1,30 @@
+const { app, BrowserWindow, ipcMain } = require('electron/main')
+const path = require('node:path')
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+        }
+    })
+
+    win.loadFile('index.html')
+}
+
+// app.whenReady().then(() => {
+//     createWindow()
+// })
+
+app.on("ready",()=>{
+    ipcMain.handle('ping', () => 'pong')
+    createWindow()
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+})
+app.on("window-all-closed",()=>{
+    console.log("All windows closed")
+    if (process.platform !== "win32")  app.quit()
+})
